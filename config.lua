e019c9a9b148e0d9af0d12ebb01944ca881c16e2
Jakob <93420584+mono-luna@users.noreply.github.com>
2023-03-30 02:16:28 -0600

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+-- General Settings --
+
+Config.sAOP = "~r~None Set" -- The default AOP when the server is up. Color Library: https://docs.fivem.net/docs/game-references/text-formatting/
+Config.sPT = false -- The starting peacetime. Set to true if you want peacetime to be enabled on launch.
+Config.Time = 15 -- How much time you want the cooldown to be (in minutes)
+
+-- Permissions --
+
+Config.AOP = '2030.staff' -- The ace permission that someone needs to use the AOP command(s). Ace Permission Guide: https://docs.vespura.com/vmenu/permissions-ref/
+Config.Priority = '2030.prio' -- The ace permission that is required to use priority command(s).
+Config.PT = '2030.staff' -- The ace permission that is required to turn peacetime on & off.
