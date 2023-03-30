50af2bca065a93b81cf68ed5369ba0c884ce73a1
Jakob <93420584+mono-luna@users.noreply.github.com>
2023-03-30 02:15:26 -0600

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+-- Made by Luna: https://github.com/mono-luna/fivem-hud-system
+
+fx_version 'bodacious'
+game 'gta5'
+
+author 'Luna'
+description 'A HUD System that consists of AOP, Priority & Peacetime'
+
+client_script "cl.lua"
+server_script "sv.lua"
+shared_script "config.lua"
